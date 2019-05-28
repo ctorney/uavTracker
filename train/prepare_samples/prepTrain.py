2c3045f94b971246a9cfb4fa866472f56cba3abb
Mixmixmix <mikolaj.kundegorski@gmail.com>
2019-05-28 12:12:53 +0530

    clarity of messages

--- train/prepare_samples/prepTrain.py
+++ train/prepare_samples/prepTrain.py
@@ -7,9 +7,10 @@ import pandas as pd
 import os,sys,glob
 import cv2
 import yaml
+import pickle
 sys.path.append('../..')
 sys.path.append('..')
-from models.yolo_models import get_yolo_model
+from models.yolo_models import get_yolo_model, get_yolo_model_feats
 from utils.decoder import decode
 
 def main(argv):
@@ -22,7 +23,6 @@ def main(argv):
     with open(root_dir + argv[2], 'r') as configfile:
         config = yaml.safe_load(configfile)
 
-
     #TODO: since this is the first file to use, maybe add a check if all directories exist?
 
     image_dir = root_dir + config['data_dir']
@@ -31,6 +31,8 @@ def main(argv):
 
     #Training type dependent
     training_type = config['training_type']
+    print("Training type is " + training_type)
+    print(config[training_type])
     your_weights = weights_dir + config[training_type]['weights']
     train_files_regex = config[training_type]['train_files_regex']
 
@@ -42,16 +44,23 @@ def main(argv):
     train_images =  glob.glob( image_dir + train_files_regex )
     annotations_file = train_dir + config['untrained_annotations_fname']
 
-    max_l=100
-    min_l=10
+    max_l=400
+    min_l=2
 
     im_size=config['IMAGE_H'] #size of training imageas for yolo
 
     ##################################################
+    print("Loading YOLO models")
     yolov3 = get_yolo_model(im_size,im_size,num_class,trainable=False)
     yolov3.load_weights(your_weights,by_name=True) #TODO is by_name necessary here?
 
+    # Creating another model to provide visualisation and/or extraction of high level features
 
+    yolov3_feats = get_yolo_model_feats(im_size,im_size,num_class,trainable=False)
+    yolov3_feats.load_weights(your_weights,by_name=True) #TODO is by_name necessary here?
+
+
+    print("YOLO models loaded, my dear.")
     ########################################
     im_num=1
     all_imgs = []
@@ -70,8 +79,6 @@ def main(argv):
             enlarged_im[:height,:width] = im.copy()
             im = enlarged_im.copy()
             height, width = im.shape[:2]
-
-        print(".")
 
         for x in np.arange(0,1+width-im_size,im_size):#'1+' added to allow case when image has exactly size of one window
             for y in np.arange(0,1+height-im_size,im_size):
@@ -94,15 +101,26 @@ def main(argv):
                 new_image = np.expand_dims(new_image, 0)
 
                 # run the prediction
-                sys.stdout.write('Y')
+                sys.stdout.write('Yolo predicting...')
                 sys.stdout.flush()
                 yolos = yolov3.predict(new_image)
-                sys.stdout.write(str(len(yolos)))
-                sys.stdout.flush()
 
-                sys.stdout.write('D')
+                # yolo_feats = yolov3_feats.predict(new_image)
+                # print(type(yolo_feats))
+                # print(type(yolo_feats[1]))
+                # print(yolo_feats[1].shape)
+                # print(yolo_feats[1].dtype)
+                # fileObject = open("feats.pickle",'wb')
+                # pickle.dump(yolo_feats[1],fileObject)
+                # fileObject.close()
+                # print("pickedleeee")
+                # cv2.imshow("heatmap",yolo_feats[1][:,:,1])
+                # k = cv2.waitKey(0)
+
+                sys.stdout.write('Decoding...')
                 sys.stdout.flush()
                 boxes = decode(yolos, obj_thresh, nms_thresh)
+                sys.stdout.write('Done!#of boxes:')
                 sys.stdout.write(str(len(boxes)))
                 sys.stdout.flush()
                 for b in boxes:
