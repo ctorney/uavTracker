db819b2fba28db88aea59a9502e3e903559f836e
Colin Torney <colin.j.torney@gmail.com>
2020-02-10 15:18:51 +0000

    use tensorflow's keras

--- train/generator.py
+++ train/generator.py
@@ -1,7 +1,7 @@
 import os, sys, cv2
 import copy
 import numpy as np
-from keras.utils import Sequence
+from tensorflow.keras.utils import Sequence
 sys.path.append("..")
 from utils.bbox import BoundBox, bbox_iou
 from utils.image import apply_random_scale_and_crop, random_distort_image, random_flip, correct_bounding_boxes, random_flip2, correct_bounding_boxes2
