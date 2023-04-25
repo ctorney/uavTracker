102eb30821a59c56e1ad65c7f305cacc94a266ba
M Kunde <mikolaj.kundegorski@gmail.com>
2023-04-25 10:43:26 +0100

    do not generate massive images

--- run_without_annotating.sh
+++ run_without_annotating.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
 set -e
 rm -rf data/toys
 rm -f weights/toys_model_1_phase_*
 cd utils
-python toy_data_generator.py -c ../experiments/toys.yml --size 2000
+python toy_data_generator.py -c ../experiments/toys.yml --size 400
 cd ../data/toys
 ln -s ../../weights .
 cd ../../train
-python train.py -c ../experiments/toys.yml
+python train.py -c ../experiments/toys.yml --test-run
 python postTrainTest.py -c ../experiments/toys.yml
