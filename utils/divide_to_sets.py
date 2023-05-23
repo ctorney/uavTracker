4f78b0b8b8412c32bedcc00ebda5427fe8d3892b
M Kunde <mikolaj.kundegorski@gmail.com>
2023-05-23 14:03:20 +0100

    divide gnu subset into train and test

---
+++ utils/divide_to_sets.py
@@ -0,0 +1,11 @@
+import os, glob, shutil
+from numpy.random import default_rng
+
+all_folder = '../data/gnu/subsets/train_old/'
+imgs_to_pull = 2000
+output_folder = '../data/gnu/subsets/test_old/'
+allfiles =glob.glob(f'{all_folder}/*.JPG')
+
+rng = default_rng()
+for iii in rng.choice(len(allfiles),imgs_to_pull, replace=False):
+    shutil.move(allfiles[iii], os.path.join(output_folder,os.path.basename(allfiles[0])))
