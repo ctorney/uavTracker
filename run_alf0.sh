edb0af4cdda666363df1478700110787b3905fbb
M Kunde <mikolaj.kundegorski@gmail.com>
2024-07-12 08:44:25 +0200

    sanity and real alf

--- run_alf0.sh
+++ run_alf0.sh
@@ -1,16 +1,16 @@
 set -e
-rm -rf data/final_alfs
-rm -f weights/alfs_terrier*
+rm -rf data/final_alfsanity
+rm -f weights/alfsanity_terrier*
 cd utils
-python toy_data_generator.py -c ../experiments/alfs.yml -g ../experiments/alfs_datasets.yml
-cd ../data/final_alfs
+python toy_data_generator.py -c ../experiments/alfsanity.yml -g ../experiments/alfsanity_datasets.yml
+cd ../data/final_alfsanity
 ln -s ../../weights .
 cd ../../train
-python train.py -c ../experiments/alfs.yml --test-run
-python postTrainTest.py -c ../experiments/alfs.yml
+python train.py -c ../experiments/alfsanity.yml --test-run
+python postTrainTest.py -c ../experiments/alfsanity.yml
 cd ../tracking
-python transforms.py -c ../experiments/alfs.yml -s
-python runTracker.py -c ../experiments/alfs.yml -s
+python transforms.py -c ../experiments/alfsanity.yml -s
+python runTracker.py -c ../experiments/alfsanity.yml -s
 cd ../deepBeastLinker
-python trainTracker.py -c ../experiments/alfs.yml
+python trainTracker.py -c ../experiments/alfsanity.yml
 
---
+++ run_alf1.sh
@@ -0,0 +1,16 @@
+set -e
+rm -rf data/final_alfs
+rm -f weights/alfs_terrier*
+cd utils
+python toy_data_generator.py -c ../experiments/alfs.yml -g ../experiments/alfs_datasets.yml
+cd ../data/final_alfs
+ln -s ../../weights .
+cd ../../train
+python train.py -c ../experiments/alfs.yml
+python postTrainTest.py -c ../experiments/alfs.yml
+cd ../tracking
+python transforms.py -c ../experiments/alfs.yml -s
+python runTracker.py -c ../experiments/alfs.yml -s
+cd ../deepBeastLinker
+python trainTracker.py -c ../experiments/alfs.yml
+
