fe76e95273cd7a5b2e9b4bdc27441751b845b2b9
M Kunde <mikolaj.kundegorski@gmail.com>
2024-07-17 06:22:52 +0200

    a script to run

--- final_squirrel_versus_terrier.sh
+++ final_squirrel_versus_terrier.sh
@@ -3,64 +3,29 @@ rm -rf data/alfs*
 #rm -f weights/alfs_*
 cd utils
 python toy_data_generator.py -c ../experiments/alfs_squirrel.yml
-python toy_data_generator.py -c ../experiments/alfs_squirrel_identical.yml
 python toy_data_generator.py -c ../experiments/alfs_terrier.yml
-python toy_data_generator.py -c ../experiments/alfs_terrier_identical.yml
 cd ../data/alfs_squirrel
 ln -s ../../weights .
 cd ../alfs_terrier
 ln -s ../../weights .
-cd ../alfs_squirrel_identical
-ln -s ../../weights .
-cd ../alfs_terrier_identical
-ln -s ../../weights .
-#squirrel
+
+#terrier
 cd ../../train
-python train.py -c ../experiments/alfs_squirrel.yml
+python train.py -c ../experiments/alfs_terrier.yml
+python postTrainTest.py -c ../experiments/alfs_terrier.yml
+cd ../deepBeastLinker
+python trainTracker.py -c ../experiments/alfs_terrier.yml
+python testTrackers.py -c ../experiments/alfs_terrier.yml
+
+#squirrel
 cd ../train
+python train.py -c ../experiments/alfs_squirrel.yml
 python postTrainTest.py -c ../experiments/alfs_squirrel.yml
 cd ../deepBeastLinker
 python trainTracker.py -c ../experiments/alfs_squirrel.yml
 python testTrackers.py -c ../experiments/alfs_squirrel.yml
 cd ../tracking
-python transforms.py -c ../experiments/alfs_squirrel.yml -s
-python runTracker.py -c ../experiments/alfs_squirrel.yml -s
-
-#terrier
-cd ../../train
-python train.py -c ../experiments/alfs_terrier.yml
-cd ../train
-python postTrainTest.py -c ../experiments/alfs_terrier.yml
-cd ../deepBeastLinker
-python trainTracker.py -c ../experiments/alfs_terrier.yml
-python testTrackers.py -c ../experiments/alfs_terrier.yml
-cd ../tracking
 python transforms.py -c ../experiments/alfs_terrier.yml -s
 python runTracker.py -c ../experiments/alfs_terrier.yml -s
-
-
-#terrier identical
-cd ../../train
-python train.py -c ../experiments/alfs_terrier_identical.yml
-cd ../train
-python postTrainTest.py -c ../experiments/alfs_terrier_identical.yml
-cd ../deepBeastLinker
-python trainTracker.py -c ../experiments/alfs_terrier_identical.yml
-python testTrackers.py -c ../experiments/alfs_terrier_identical.yml
-cd ../tracking
-python transforms.py -c ../experiments/alfs_terrier_identical.yml -s
-python runTracker.py -c ../experiments/alfs_terrier_identical.yml -s
-
-
-#squirrel identical
-cd ../../train
-python train.py -c ../experiments/alfs_squirrel_identical.yml
-cd ../train
-python postTrainTest.py -c ../experiments/alfs_squirrel_identical.yml
-cd ../deepBeastLinker
-python trainTracker.py -c ../experiments/alfs_squirrel_identical.yml
-python testTrackers.py -c ../experiments/alfs_squirrel_identical.yml
-cd ../tracking
-python transforms.py -c ../experiments/alfs_squirrel_identical.yml -s
-python runTracker.py -c ../experiments/alfs_squirrel_identical.yml -s
-
+python transforms.py -c ../experiments/alfs_squirrel.yml -s
+python runTracker.py -c ../experiments/alfs_squirrel.yml -s
