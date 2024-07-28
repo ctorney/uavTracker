38a6a5c6aa5ae10440c29a46d7e7b65a87f23663
M Kunde <mikolaj.kundegorski@gmail.com>
2024-07-28 07:57:19 +0200

    just rerun testing of deepbeastpredictor

--- final_squirrel_versus_terrier.sh
+++ final_squirrel_versus_terrier.sh
@@ -1,6 +1,5 @@
 set -e
 rm -rf data/alfs*
-#rm -f weights/alfs_*
 cd utils
 python toy_data_generator.py -c ../experiments/alfs_squirrel.yml
 python toy_data_generator.py -c ../experiments/alfs_terrier.yml
@@ -10,22 +9,6 @@ cd ../alfs_terrier
 ln -s ../../weights .
 
 #terrier
-cd ../../train
-python train.py -c ../experiments/alfs_terrier.yml
-python postTrainTest.py -c ../experiments/alfs_terrier.yml
-cd ../deepBeastLinker
-python trainTracker.py -c ../experiments/alfs_terrier.yml
+cd ../../deepBeastLinker
 python testTrackers.py -c ../experiments/alfs_terrier.yml
-
-#squirrel
-cd ../train
-python train.py -c ../experiments/alfs_squirrel.yml
-python postTrainTest.py -c ../experiments/alfs_squirrel.yml
-cd ../deepBeastLinker
-python trainTracker.py -c ../experiments/alfs_squirrel.yml
 python testTrackers.py -c ../experiments/alfs_squirrel.yml
-cd ../tracking
-python transforms.py -c ../experiments/alfs_terrier.yml -s
-python runTracker.py -c ../experiments/alfs_terrier.yml -s
-python transforms.py -c ../experiments/alfs_squirrel.yml -s
-python runTracker.py -c ../experiments/alfs_squirrel.yml -s
